b535ec9eb3fdde1e1b20183dd945cdb28bf7b0c4
Cristian Gutierrez <fistergutierrez@gmail.com>
2023-03-27 09:53:22 -0600

    actualizacion variables basicas

---
+++ variables/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var myIntVar int
+	myIntVar = -12
+	fmt.Println("My variable is: ", myIntVar)
+
+	var myUintVar uint
+	myIntVar = 12
+	fmt.Println("My variable is: ", myUintVar)
+
+	var myStringVar string
+	myStringVar = "My String variable"
+	fmt.Println("My variable is: ", myStringVar)
+
+	var myBoolVar bool
+	myBoolVar = true
+	fmt.Println("My variable is: ", myBoolVar)
+}
